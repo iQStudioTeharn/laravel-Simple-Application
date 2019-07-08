07b52e484d8faa50b224c0e8d4024af32d5724d4
alireza farokhi <alireza.farokhi70@gmail.com>
2019-07-08 13:37:45 +0430

    disqus pakage added for commenting system

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -51,7 +51,7 @@
 
                 <!-- Comments Form -->
                 
-                @if (Auth::check())
+                {{-- @if (Auth::check())
                     <hr>
                     <div class="well">
                         <h4>Leave a Comment:</h4>
@@ -171,8 +171,30 @@
                         
 
                 @endif
+--}}
 
-            </div>
+<div id="disqus_thread"></div>
+<script>
+
+/**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+/*
+var disqus_config = function () {
+this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+*/
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://cms-me.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
+            </div> 
 
             <!-- Blog Sidebar Widgets Column -->
             <div class="col-md-4">
